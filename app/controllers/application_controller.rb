315500bfc5e1f8a8decd586dadaad63708dc6474
mr110825 <ptin110825@gmail.com>
2021-09-27 04:46:06 +0900

    Add:403ページ追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   include Pundit
-#   rescue_from Pundit::NotAuthorizedError, with: :render_403
+  rescue_from Pundit::NotAuthorizedError, with: :render_403
 
   def render_403
     render template: 'users/403', status: 403
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -4,7 +4,7 @@ class UserPolicy < ApplicationPolicy
     end
   
     def show?
-      true
+      user.admin?
     end
   
     def create?
